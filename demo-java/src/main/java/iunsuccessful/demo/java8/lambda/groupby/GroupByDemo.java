aaad0be2775f2ad04be49b29ef2f3ed4202db56c
LiQZ <lzq@azt365.com>
2017-07-18 15:34:55 +0800

    lambda group by 部分演示

--- demo-java/src/main/java/iunsuccessful/demo/java8/lambda/groupby/GroupByDemo.java
+++ demo-java/src/main/java/iunsuccessful/demo/java8/lambda/groupby/GroupByDemo.java
@@ -3,9 +3,15 @@ package iunsuccessful.demo.java8.lambda.groupby;
 import iunsuccessful.demo.common.utils.DataUtils;
 import iunsuccessful.demo.common.utils.PrintUtils;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
-import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.*;
 
 /**
  * 分组
@@ -14,9 +20,15 @@ import static java.util.stream.Collectors.groupingBy;
 public class GroupByDemo {
 
     public static void main(String[] args) {
-        groupBy1();
+//        groupBy1();
+//        groupBy2();
+//        groupBy3();
+        groupBy4();
     }
 
+    /**
+     * 统计相同出现字母
+     */
     private static void groupBy1() {
         List<String> list = DataUtils.getRandomList(10);
         PrintUtils.print(list);
@@ -24,6 +36,45 @@ public class GroupByDemo {
             System.out.printf("########### %s ##########\n", s);
             PrintUtils.print(strings);
         });
+    }
+
+    /**
+     * 统计相同开头字母的出现次数
+     */
+    private static void groupBy2() {
+        List<String> list = DataUtils.getRandomList(10);
+        PrintUtils.print(list);
+        list.stream().collect(groupingBy(s -> s.charAt(0), Collectors.counting())).forEach(new BiConsumer<Character, Long>() {
+            @Override
+            public void accept(Character character, Long aLong) {
+                System.out.printf("character %s count %d\n", character, aLong);
+            }
+        });
+    }
+
+    /**
+     * 对应字符
+     */
+    private static void groupBy3() {
+        List<String> list = DataUtils.getRandomList(10);
+        PrintUtils.print(list);
+        list.stream().collect(groupingBy(s -> s.charAt(0), mapping(s -> s, joining(";")))).forEach(new BiConsumer<Character, String>() {
+            @Override
+            public void accept(Character character, String s) {
+                System.out.printf("character %s s %s\n", character, s);
+            }
+        });
+    }
+
+    /**
+     * 相同字符最多的那个
+     */
+    private static void groupBy4() {
+        List<String> list = DataUtils.getRandomList(10);
+        PrintUtils.print(list);
+        Optional<Character> c =  list.stream().collect(groupingBy(s -> s.charAt(0), counting())).entrySet()
+                .stream().max(Map.Entry.comparingByValue()).map(Map.Entry::getKey);
+        System.out.println(c.orElse('\n'));
     }
 
 }
