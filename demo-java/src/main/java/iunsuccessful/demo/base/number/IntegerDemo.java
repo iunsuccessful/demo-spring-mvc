e27947cae542d4dc35307e6e9ed959a60b0bf0a7
LiQZ <lzq@azt365.com>
2017-12-20 14:03:11 +0800

    增加示例

---
+++ demo-java/src/main/java/iunsuccessful/demo/base/number/IntegerDemo.java
@@ -0,0 +1,20 @@
+package iunsuccessful.demo.base.number;
+
+import org.apache.commons.lang3.math.NumberUtils;
+
+/**
+ * Created by LiQZ on 2017/11/18.
+ */
+public class IntegerDemo {
+
+    public static void main(String[] args) {
+        try {
+            if (NumberUtils.isNumber("13601130005")) {
+                new Integer("13601130005");
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
+}
---
+++ demo-java/src/main/java/iunsuccessful/demo/base/operation/SingleDemo.java
@@ -0,0 +1,16 @@
+package iunsuccessful.demo.base.operation;
+
+/**
+ * 单运算的运用
+ * Created by LiQZ on 2017/11/16.
+ */
+public class SingleDemo {
+
+    public static void main(String[] args) {
+        int i = 6;
+        System.out.println(i&1);
+        System.out.println(i&2);
+        System.out.println(i&4);
+    }
+
+}
