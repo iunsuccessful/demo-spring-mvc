3af8ef9f519a24c4b86fb82eb2326614646a9a42
依韵 <n_syiyun@3songshu.com>
2019-05-23 14:53:59 +0800

    日期比较的快速方法

---
+++ demo-java/src/main/java/iunsuccessful/demo/base/date/DateCompareDemo.java
@@ -0,0 +1,9 @@
+package iunsuccessful.demo.base.date;
+
+public class DateCompareDemo {
+
+    public static void main(String[] args) {
+        System.out.println("2019-05-23 00:00:00".compareTo("2019-05-24 00:00:00") >= 0);
+    }
+
+}
