c1d49710701cef5dd4fb27a0d311cce60104f7b6
依韵 <n_syiyun@3songshu.com>
2022-06-25 16:07:41 +0800

    增加示例

--- demo-java/src/main/java/iunsuccessful/demo/base/list/CountElementInListDemo.java
+++ demo-java/src/main/java/iunsuccessful/demo/base/list/CountElementInListDemo.java
@@ -6,6 +6,8 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * 依韵 2022/6/24
@@ -20,6 +22,8 @@ public class CountElementInListDemo {
                 "expect4", "expect4", "expect4", "expect4");
 
         System.out.println(countByForEachLoopWithMapCompute(INPUT_LIST));
+        System.out.println(countByStreamToMap(INPUT_LIST));
+        System.out.println(countByStreamGroupBy(INPUT_LIST));
 
     }
 
@@ -27,6 +31,18 @@ public class CountElementInListDemo {
         Map<T, Long> resultMap = new HashMap<>();
         inputList.forEach(e -> resultMap.compute(e, (k, v) -> v == null ? 1L : v + 1L));
         return resultMap;
+    }
+
+    /**
+     * 使用流的方式，统计重复值
+     */
+    public static <T> Map<T, Long> countByStreamToMap(List<T> inputList) {
+        // keyMapper, valueMapper, mergeFunction
+        return inputList.stream().collect(Collectors.toMap(Function.identity(), v -> 1L, Long::sum));
+    }
+
+    public static  <T> Map<T, Long> countByStreamGroupBy(List<T> inputList) {
+        return inputList.stream().collect(Collectors.groupingBy(k -> k, Collectors.counting()));
     }
 
 }
--- demo-java/src/main/java/iunsuccessful/demo/java8/MapDemo.java
+++ demo-java/src/main/java/iunsuccessful/demo/java8/MapDemo.java
@@ -12,11 +12,16 @@ public class MapDemo {
         Map<String, Integer> map = new HashMap<>();
 
         // 如果 key 在 map 中不存在，就在 map 里加入 key
-        map.computeIfAbsent("key", k -> 1);
-        map.computeIfAbsent("key", k -> 1);
+        map.computeIfAbsent("key", key -> 1);
+        map.computeIfAbsent("key", key -> 1);
         // 如果 key 在 map 中不存在，就在 map 里加入 key，如果存在，就 value + 1
-        map.compute("key1", (k, v) -> v == null ? 1 : v + 1);
-        map.compute("key1", (k, v) -> v == null ? 1 : v + 1);
+        map.compute("key1", (key, oldValue) -> oldValue == null ? 1 : oldValue + 1);
+        map.compute("key1", (key, oldValue) -> oldValue == null ? 1 : oldValue + 1);
+
+        // 没有值的时候，起用用的 3
+        map.merge("key2", 3, (oldValue, newValue) -> newValue + 1);
+        // 有值的话，会用 5 + 1
+        map.merge("key2", 5, (oldValue, newValue) -> oldValue + newValue + 1);
 
         System.out.println(map);
 
